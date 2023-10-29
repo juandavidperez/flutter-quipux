122cab0a56f8a5e7bb6d9fc506af411827b62417
Juan David Perez <juandavidperezvergara@gmail.com>
2023-10-29 13:23:29 -0500

    Añadiendo navegacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'NuevaPantalla.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,21 +31,12 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  bool _isDropdownOpen = false;
-  int _selectedIndex = 2; // Inicialmente seleccionado materias
 
-  List<String> dropdownList = ['Estudiantes', 'Grupos', 'Materias', 'Evaluaciones', 'Informes'];
-
-  void toggleDropdown() {
-    setState(() {
-      _isDropdownOpen = !_isDropdownOpen;
-    });
-  }
-
-  void handleSelection(int index) {
-    setState(() {
-      _selectedIndex = index; // Actualiza el índice seleccionado
-    });
+  void _navigateToNuevaPantalla(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => NuevaPantalla()),
+    );
   }
 
   @override
@@ -54,66 +46,29 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.deepPurple,
         title: Text(widget.title),
       ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-            color: Colors.white, // Color de fondo blanco
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Colegio Quipux',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
-                  ),
-                  Container(
-                    width: 300, // Ancho deseado de la imagen
-                    height: 200, // Altura deseada de la imagen
-                    child: Image.asset('assets/images/quipux.png'), // Muestra la imagen debajo del texto
-                  ),
-                ],
-              ),
-            ),
-          ),
-          if (_isDropdownOpen)
-            Positioned(
-              top: 50, // Ajusta la posición vertical de la lista desplegable
-              left: 0, // Ajusta la posición horizontal de la lista desplegable
-              child: Container(
-                width: 200, // Ancho fijo de la lista desplegable
-                height: MediaQuery.of(context).size.height, // Altura al 100% de la pantalla
-                color: Color(0xFF6A737C), // Color de fondo predeterminado
-                child: ListView(
-                  children: dropdownList.asMap().entries.map((entry) {
-                    int index = entry.key;
-                    String value = entry.value;
-                    return Container(
-                      color: _selectedIndex == index ? Color(0xFFC8CCD0) : Color(0xFF6A737C),
-                      child: ListTile(
-                        title: Text(
-                          value,
-                          style: TextStyle(
-                            color: _selectedIndex == index ? Color(0xFF555A5E) : Colors.white,
-                          ),
-                        ),
-                        onTap: () {
-                          handleSelection(index);
-                          // Aquí puedes agregar la lógica para manejar la selección de cada elemento de la lista
-                        },
-                      ),
-                    );
-                  }).toList(),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: GestureDetector(
+            onTap: () {
+              _navigateToNuevaPantalla(context); // Navegar a la nueva pantalla al hacer clic en cualquier lugar
+            },
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Colegio Quipux',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
                 ),
-              ),
-            ),
-          Container(
-            alignment: Alignment.topLeft,
-            child: IconButton(
-              icon: Icon(Icons.menu),
-              onPressed: toggleDropdown,
+                Container(
+                  width: 300,
+                  height: 200,
+                  child: Image.asset('assets/images/quipux.png'),
+                ),
+              ],
             ),
           ),
-        ],
+        ),
       ),
     );
   }
